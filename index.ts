32f220528e9d231bb1eb3f780ef9d9cc6dfb903d
Drew Davis <drewdavis@hey.com>
2023-12-23 04:10:15 -0500

    init commit

---
+++ index.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import path from "path";
+import { contentType } from "mime-types";
+import { Plugin } from "vite";
+
+export type Config = {
+  readonly pattern: RegExp;
+  readonly resolve: string | ((path: string, groups: string[]) => string);
+}[];
+
+export default function serveStatic(config: Config): Plugin {
+  return {
+    name: "serve-static",
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (!req.url) {
+          return next();
+        }
+
+        for (const { pattern, resolve } of config) {
+          const match = pattern.exec(req.url);
+
+          if (match) {
+            const filePath =
+              typeof resolve === "string" ? resolve : resolve(req.url, match.slice(1));
+            const stats = fs.statSync(filePath, { throwIfNoEntry: false });
+
+            if (!stats || !stats.isFile()) {
+              res.writeHead(404);
+              res.end("Not found");
+              console.error(`File ${filePath} is not a file`);
+              return;
+            }
+
+            const type = contentType(path.basename(filePath));
+            res.writeHead(200, {
+              "Content-Length": stats.size,
+              "Content-Type": type ? type : undefined,
+            });
+
+            const stream = fs.createReadStream(filePath);
+            stream.pipe(res);
+            return;
+          }
+        }
+
+        return next();
+      });
+    },
+  };
+}
